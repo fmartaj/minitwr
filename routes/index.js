e25d42343ff5e158baaf325ed389c962aec4c6f3
Gomes Fernandes Caty <caty.gomes-fernandes@turing.u-strasbg.fr>
2015-05-06 17:58:58 +0200

    caty messed up some of fati’s files sorry :(

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
+/* code d'avant 
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Peep' });
+});
+
+module.exports = router;
+*/
+
+
+var express = require('express');
+var router = express.Router();
+
+var tweets = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Banane' });
+  res.render('index', { title: 'Express', tweets: tweets });
+});
+
+router.post('/', function(req, res, next) {
+    tweets.unshift(req.body.tweet);
+    res.redirect('/')
 });
 
 module.exports = router;
